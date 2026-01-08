dc8f56dc1919c1501af27bc8de9cf81776226857
Sidharth Sunil <sidharthsunil1305@gmail.com>
2026-01-08 20:17:05 +0530

    Fix Railway MySQL env variables

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ import os
 
 SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")
 
-MYSQL_HOST = os.getenv("MYSQLHOST")
-MYSQL_USER = os.getenv("MYSQLUSER")
-MYSQL_PASSWORD = os.getenv("MYSQLPASSWORD")
-MYSQL_DB = os.getenv("MYSQLDATABASE")
-MYSQL_PORT = int(os.getenv("MYSQLPORT", 3306))
+MYSQL_HOST = os.getenv("MYSQL_HOST")
+MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+MYSQL_DB = os.getenv("MYSQL_DATABASE")
+MYSQL_PORT = int(os.getenv("MYSQL_PORT", 3306))
 
 MYSQL_CURSORCLASS = "DictCursor"
 
@@ -14,3 +14,4 @@ RAZORPAY_KEY_ID = os.getenv("RAZORPAY_KEY_ID")
 RAZORPAY_SECRET = os.getenv("RAZORPAY_SECRET")
 
 ADMIN_EMAIL = os.getenv("ADMIN_EMAIL", "terminalplate@gmail.com")
+
