cb5de3c37809f88b1b8aae80f4b805f82e663474
Sweyy-goat <sidharthsunil1305@gmail.com>
2026-01-09 14:46:03 +0530

    Update db.py

--- utils/db.py
+++ utils/db.py
@@ -1,2 +1,7 @@
 from flask_mysqldb import MySQL
 mysql = MySQL()
+@app.before_request
+def set_timezone():
+    cur = mysql.connection.cursor()
+    cur.execute("SET time_zone = '+05:30'")
+    cur.close()
