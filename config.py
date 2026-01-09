a01cd12d8ce81eaca660b0d9300f8c04e2b1cbf6
Sweyy-goat <sidharthsunil1305@gmail.com>
2026-01-09 12:59:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")
 MYSQL_HOST = os.getenv("MYSQL_HOST")
 MYSQL_USER = os.getenv("MYSQL_USER")
 MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
-MYSQL_DB = os.getenv("MYSQL_DATABASE")
+MYSQL_DB = os.getenv("MYSQL_DB")
 MYSQL_PORT = int(os.getenv("MYSQL_PORT", 3306))
 
 MYSQL_CURSORCLASS = "DictCursor"
