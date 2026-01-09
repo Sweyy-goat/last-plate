b73d8c47d23670fb2ad39dba2533408d31cf4caa
Sweyy-goat <sidharthsunil1305@gmail.com>
2026-01-09 14:54:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from utils.db import mysql
 
 app = Flask(__name__)
@@ -7,24 +7,26 @@ app.config.from_pyfile("config.py")
 mysql.init_app(app)
 app.secret_key = app.config["SECRET_KEY"]
 
+# 🔥 FORCE MYSQL TIMEZONE TO IST
+with app.app_context():
+    from utils.db import set_mysql_timezone
+    set_mysql_timezone()
+
 from routes.auth import auth_bp
 app.register_blueprint(auth_bp)
+
 from routes.restaurant import restaurant_bp
 app.register_blueprint(restaurant_bp)
 
 from routes.browse import browse_bp
 app.register_blueprint(browse_bp)
+
 from routes.order import order_bp
 app.register_blueprint(order_bp)
-
-
-from flask import render_template
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-
 if __name__ == "__main__":
     app.run()
-
