d295f0c1ecc4a0319a93e0ab540b0cc0976b209b
Sweyy-goat <sidharthsunil1305@gmail.com>
2026-01-11 21:19:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ MYSQL_PORT = int(os.getenv("MYSQL_PORT", 3306))
 
 MYSQL_CURSORCLASS = "DictCursor"
 
-RAZORPAY_KEY_ID = os.getenv("RAZORPAY_KEY_ID")
-RAZORPAY_SECRET = os.getenv("RAZORPAY_SECRET")
+RAZORPAY_KEY_ID = os.getenv("rzp_test_S2bmctosdabOoM")
+RAZORPAY_SECRET = os.getenv("77RfmWU2bJoKhsAknuxti9bi")
 
 ADMIN_EMAIL = os.getenv("ADMIN_EMAIL", "terminalplate@gmail.com")
 
