de01f05911c369a3343a779dc5edc9468a9f23af
Sweyy-goat <sidharthsunil1305@gmail.com>
2026-03-06 12:47:40 +0530

    Update db.py

--- utils/db.py
+++ utils/db.py
@@ -1,9 +1,11 @@
 from flask_mysqldb import MySQL
+
 mysql = MySQL()
 
 def set_mysql_timezone():
-    cur = mysql.connection.cursor()
-    cur.execute("SET time_zone = '+05:30'")
-    cur.close()
-
-
+    try:
+        cur = mysql.connection.cursor()
+        cur.execute("SET time_zone = '+05:30'")
+        cur.close()
+    except Exception as e:
+        print("MySQL timezone setup skipped:", e)
